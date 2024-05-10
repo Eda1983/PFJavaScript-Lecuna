0c6d5bb71f1607441d951406187d1f2fea0b6400
Edith Lecuna <edithlecuna@gmail.com>
2024-05-09 21:52:01 -0300

    json

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 // Obtener elementos del DOM
 const contenedorProductos = document.querySelector("#productos");
 const carritoVacio = document.querySelector("#carrito-vacio");
-const carritoProductos = document.querySelector("#carrito-productos");
+const carritoProductos = document.querySelector("#carrito");
 const carritoTotal = document.querySelector("#carrito-total");
 const numerito = document.querySelector("#numerito");
+
+// Inicialización del carrito
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+// Definición de productos
+const productos = [
+    {
+        id: "Amigurumi1",
+        titulo: "Rino",
+        precio: 2000,
+        img: "./img/rino.jpg",
+    },
+    {
+        id: "Amigurumi2",
+        titulo: "Jirafa",
+        precio: 2000,
+        img: "./img/jirafi.jpg",
+    },
+    {
+        id: "Amigurumi3",
+        titulo: "Koala",
+        precio: 2000,
+        img: "./img/koala.jpg",
+    }
+];
 
 // Función para crear un elemento de producto
 function crearProductoElemento(producto) {
@@ -13,30 +38,28 @@ function crearProductoElemento(producto) {
         <img class="producto-img" src="${producto.img}" alt="${producto.titulo}">
         <h3>${producto.titulo}</h3>
         <p>$${producto.precio}</p>
-        <button class="boton-agregar" id=${producto.id}>Agregar al carrito</button>
+        <button class="producto-btn" data-id="${producto.id}">Agregar al carrito</button>
     `;
-    
-    const btn = document.createElement("button");
-    btn.classList.add("producto-btn");
-    btn.innerText = "Agregar al carrito";
-    btn.addEventListener("click", () => {
-        agregarAlCarrito(producto);
+
+    div.querySelector(".producto-btn").addEventListener("click", (e) => {
+        const id = e.target.dataset.id;
+        const productoAsignado = productos.find(prod => prod.id === id);
+        agregarAlCarrito(productoAsignado);
     });
-    
-    div.appendChild(btn);
+
     return div;
 }
 
 // Función para actualizar el carrito
 function actualizarCarrito() {
+    carritoProductos.innerHTML = ""; // Limpiar el contenido del carrito
+
     if (carrito.length === 0) {
         carritoVacio.classList.remove("d-none");
-        carritoProductos.classList.add("d-none");
+        carritoProductos.classList.add("d-none"); // Ocultar el contenedor de productos del carrito
     } else {
         carritoVacio.classList.add("d-none");
-        carritoProductos.classList.remove("d-none");
-
-        carritoProductos.innerHTML = "";
+        carritoProductos.classList.remove("d-none"); // Mostrar el contenedor de productos del carrito
         carrito.forEach(producto => {
             const div = document.createElement("div");
             div.classList.add("carrito-producto");
@@ -45,18 +68,19 @@ function actualizarCarrito() {
 
             div.innerHTML = `
                 <p>${producto.titulo} | Precio: $${producto.precio} | Cantidad: ${producto.cantidad} | Subtotal: $${subtotal}</p>
-                <button class="carrito-producto-btn">⬇️</button>
-                <button class="carrito-producto-btn">⬆️</button>
-                <button class="carrito-producto-btn">✖️</button>
+                <button class="carrito-producto-btn" data-action="restar">⬇️</button>
+                <button class="carrito-producto-btn" data-action="sumar">⬆️</button>
+                <button class="carrito-producto-btn" data-action="borrar">✖️</button>
             `;
 
             div.querySelectorAll(".carrito-producto-btn").forEach(btn => {
                 btn.addEventListener("click", () => {
-                    if (btn.innerText === "⬇️") {
+                    const action = btn.dataset.action;
+                    if (action === "restar") {
                         restarDelCarrito(producto);
-                    } else if (btn.innerText === "⬆️") {
+                    } else if (action === "sumar") {
                         sumarDelCarrito(producto);
-                    } else if (btn.innerText === "✖️") {
+                    } else if (action === "borrar") {
                         borrarDelCarrito(producto);
                     }
                 });
@@ -70,50 +94,49 @@ function actualizarCarrito() {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-// Event listener para botones de agregar al carrito
-contenedorProductos.addEventListener("click", (e) => {
-    if (e.target && e.target.classList.contains("boton-agregar")) {
-        const id = e.target.id;
-        const productoAsignado = productos.find(prod => prod.id === id);
-        agregarAlCarrito(productoAsignado);
-    }
-});
-
 // Función para agregar un producto al carrito
 function agregarAlCarrito(producto) {
-    const itemEncontrado = carrito.find(item => item.titulo === producto.titulo);
+    const itemEncontrado = carrito.find(item => item.id === producto.id);
     if (itemEncontrado) {
-        itemEncontrado.cantidad++;
+        if (itemEncontrado.cantidad < 1000) { // Límite máximo de 1000 artículos en el carrito
+            itemEncontrado.cantidad++;
+        } else {
+            // Mostrar un mensaje de advertencia si se alcanza el límite máximo
+            Toastify({
+                text: "Se ha alcanzado el límite máximo de artículos en el carrito (1000).",
+                gravity: "bottom",
+                position: "right",
+                duration: 3000
+            }).showToast();
+        }
     } else {
         carrito.push({...producto, cantidad: 1});
     }
 
-    actualizarCarrito();
+    actualizarCarrito(); // Llamar a actualizarCarrito() después de agregar un producto
 
+    // Mostrar notificación de producto agregado al carrito
     Toastify({
         text: "Producto agregado al carrito.",
-        gravity: "bottom", // top o bottom
-        position: "right", // left, center o right
+        gravity: "bottom",
+        position: "right",
         duration: 1000
     }).showToast();
 }
 
+
 // Función para borrar un producto del carrito
 function borrarDelCarrito(producto) {
-    const idProducto = producto.id;
-    const index = carrito.findIndex(item => item.id === idProducto);
-    if (index !== -1) {
-        carrito.splice(index, 1);
-        actualizarCarrito();
-    } else {
-        console.error("Producto no encontrado en el carrito:", producto);
-    }
+    carrito = carrito.filter(item => item.id !== producto.id);
+    actualizarCarrito();
 }
 
 // Función para restar la cantidad de un producto en el carrito
 function restarDelCarrito(producto) {
-    if (producto.cantidad !== 1) {
+    if (producto.cantidad > 1) {
         producto.cantidad--;
+    } else {
+        borrarDelCarrito(producto);
     }
     actualizarCarrito();
 }
@@ -136,33 +159,66 @@ function calcularNumerito() {
     numerito.innerText = numeritoTotal;
 }
 
-// Inicialización
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+// Función para manejar el evento de clic en el botón "Finalizar compra"
+document.getElementById("finalizar-compra").addEventListener("click", () => {
+    // Aquí va el código para mostrar la alerta de SweetAlert
+    Swal.fire({
+        title: '¡Gracias por tu compra!',
+        text: '¡Esperamos verte de nuevo pronto!',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+    });
+});
 
-const productos = [
-    {
-        id: "Amigurumi 1",
-        titulo: "Rino",
-        precio: 2000,
-        img: "./img/rino.jpg",
-    },
-    {
-        id: "Amigurumi 2",
-        titulo: "Jirafi",
-        precio: 2000,
-        img: "./img/jirafi.jpg",
-    },
-    {
-        id: "Amigurumi 3",
-        titulo: "Koala",
-        precio: 2000,
-        img: "./img/koala.jpg",
-    }
-];
 
+// Inicialización de la página
 productos.forEach((producto) => {
     contenedorProductos.appendChild(crearProductoElemento(producto));
 });
 
 actualizarCarrito();
+
+
+fetch('data/productos.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('No se pudo cargar el archivo productos.json');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Aquí puedes trabajar con los datos cargados, como mostrarlos en tu tienda en línea
+    console.log(data); // Solo para verificar en la consola que los datos se hayan cargado correctamente
+    
+    // Iterar sobre el array de productos
+    data.forEach(producto => {
+        // Crear un elemento div para el producto
+        const productoElemento = document.createElement("div");
+        productoElemento.classList.add("producto");
+
+        // Construir el contenido HTML del producto
+        productoElemento.innerHTML = `
+            <img class="producto-img" src="${producto.img}" alt="${producto.titulo}">
+            <h3>${producto.titulo}</h3>
+            <p>Precio: $${producto.precio}</p>
+            <p>${producto.descripcion}</p>
+            <button class="producto-btn" data-id="${producto.id}">Agregar al carrito</button>
+        `;
+
+        // Agregar un evento click al botón de agregar al carrito
+        const botonAgregar = productoElemento.querySelector(".producto-btn");
+        botonAgregar.addEventListener("click", () => {
+            // Aquí puedes agregar la lógica para agregar el producto al carrito
+            agregarAlCarrito(producto);
+        });
+
+        // Agregar el elemento del producto al contenedor de productos
+        contenedorProductos.appendChild(productoElemento);
+    });
+  })
+  .catch(error => {
+    console.error('Error al cargar los datos:', error);
+  });
+
+
 
